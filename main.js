319e542cf268c9d67d949f3cf3eedf132a43287e
Guilherme <guilhermecorsino21@gmail.com>
2022-10-17 17:31:25 -0300

    creating the arrays

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const main = () => {
+  let teamOneScore;
+  let teamTwoScore;
+  let teamThreeScore;
+  let teamFourScore;
+  let equalScoreTeamOne;
+  let equalScoreTeamTwo;
+  let penalsTeamOne;
+  let penalsTeamTwo;
+
+  let GrupaA = [];
+  let GrupaB = [];
+  let GrupaC = [];
+  let GrupaD = [];
+  let GrupaE = [];
+  let GrupaF = [];
+  let GrupaG = [];
+  let GrupaH = [];
+
+  const allGroup = [];
+
+  // ELIMINATION GROUPS
+
+  let groupA = [];
+  let groupB = [];
+  let groupC = [];
+  let groupD = [];
+  let groupE = [];
+  let groupF = [];
+  let groupG = [];
+  let groupH = [];
+
+  let eliminationGroupOne = [];
+  let eliminationGroupTwo = [];
+  let quaterFinalOne = [];
+  let quaterFinalTwo = [];
+  let finalGameTeams = [];
+  let winner = [];
+
+  
